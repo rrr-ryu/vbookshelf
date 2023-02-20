5025ac6b0aff24bb761d441ede9504f9d09e5f28
rrr-ryu <zero.requiem910219@gmail.com>
2023-02-20 22:51:26 +0900

    修正

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -190,3 +190,24 @@
     </div>
   </section>
 </x-app-layout>
+
+<script>
+  // ポップアップ動作
+  const elements = document.getElementsByClassName('element');
+  const overlay = document.querySelector('#overlay');
+  const modal = document.querySelector('#modal')
+  const hidden = document.querySelector('#hidden')
+  
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', function() {
+      hidden.innerHTML = this.innerHTML;
+      overlay.style.display = 'flex';
+    });
+  }
+  
+  overlay.addEventListener('click', (e) => {
+    if (e.target === overlay) {
+      overlay.style.display = 'none';
+    }
+  });
+</script>
